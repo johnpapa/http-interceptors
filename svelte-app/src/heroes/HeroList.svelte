6ea4ace7e396efd1911e275bf6fe0290a9e698db
John Papa <john@johnpapa.net>
2021-04-02 23:51:06 -0400

    heroes list wont show if not logged in

--- svelte-app/src/heroes/HeroList.svelte
+++ svelte-app/src/heroes/HeroList.svelte
@@ -35,29 +35,31 @@
   {#if !heroes.length && !errorMessage}
     <div>Loading data ...</div>
   {/if}
-  <ul class="list">
-    {#each heroes as { id, name, description }, i (id)}
-      <li role="presentation">
-        <div class="card">
-          <CardContent {name} {description} />
-          <footer class="card-footer">
-            <ButtonFooter
-              {...deleteOptions}
-              dataId={id}
-              dataIndex={i}
-              item={heroes[i]}
-              on:clicked={() => deleteHero(heroes[i])}
-            />
-            <ButtonFooter
-              {...editOptions}
-              dataId={id}
-              dataIndex={i}
-              item={heroes[i]}
-              on:clicked={() => selectHero(heroes[i])}
-            />
-          </footer>
-        </div>
-      </li>
-    {/each}
-  </ul>
+  {#if heroes.length && !errorMessage}
+    <ul class="list">
+      {#each heroes as { id, name, description }, i (id)}
+        <li role="presentation">
+          <div class="card">
+            <CardContent {name} {description} />
+            <footer class="card-footer">
+              <ButtonFooter
+                {...deleteOptions}
+                dataId={id}
+                dataIndex={i}
+                item={heroes[i]}
+                on:clicked={() => deleteHero(heroes[i])}
+              />
+              <ButtonFooter
+                {...editOptions}
+                dataId={id}
+                dataIndex={i}
+                item={heroes[i]}
+                on:clicked={() => selectHero(heroes[i])}
+              />
+            </footer>
+          </div>
+        </li>
+      {/each}
+    </ul>
+  {/if}
 </div>
