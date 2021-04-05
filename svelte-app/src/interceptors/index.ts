f7b26f71c1688eeeea7a8dc9fba5c9c56b06c85e
John Papa <john@johnpapa.net>
2021-04-04 22:33:03 -0400

    Interceptor / Axios code is  confusing to read, since we  think of the order that we add them as being how they are applied.  *    So we  *      1. add the functions that apply the interceptors to an array in the order we want them to be applied  *      2. reverse the array, to get them how Axios interceptors wants them  *      3. execute the functions that apply the interceptors

--- svelte-app/src/interceptors/index.ts
+++ svelte-app/src/interceptors/index.ts
@@ -15,57 +15,68 @@ import { transformInterceptor } from './transform.interceptor';
  *    then requests will flow in C->b->A and responses will flow out A->B->C.
  *    Therefore, it is important to add them in the reverse of the
  *    order you want them to execute.
+ *    ... but ...
+ *    This might make the code confusing to read, since we
+ *    think of the order that we add them as being how they are applied.
+ *    So we
+ *      1. add the functions that apply the interceptors to an array in the order we want them to be applied
+ *      2. reverse the array, to get them how Axios interceptors wants them
+ *      3. execute the functions that apply the interceptors
  */
 export function applyHttpInterceptors() {
   globalHeaders();
 
-  /**
-   * Transform Response:
-   *    Transform the response, making it easier to consume.
-   *    This could happen anywhere in this particular stream,
-   *    as it operates on the response.
-   */
-  transformInterceptor();
-  /**
-   *  Busy:
-   *    Enable and increment the count of HTTP requests, which can be used to show a busy indicator.
-   *    Also decrement the count when responses are received, to eventually turn off the busy indicator.
-   *    This must happen once it is certain a request will be made,
-   *    and right after the response is received.
-   */
-  busyInterceptor();
-  /**
-   *  Log headers:
-   *    Log all headers.
-   *    Must come after the headers are stuffed and before the request is made.
-   */
-  logHeadersInterceptor();
-  /**
-   * CSRF:
-   *    Add the CSRF headers.
-   */
-  csrfInterceptor();
-  /**
-   * Auth:
-   *    Add the authentication headers.
-   */
-  authInterceptor();
-  /**
-   SSL:
-   *    Ensure SSL by making calls that use http instead use https.
-   */
-  ensureSSLInterceptor();
-  /**
-   * ReadOnly:
-   *    This makes sure that HTTP POST, PUT and DELETE are not allowed if the
-   *    user does not have permission. If they do not, then it cancels the request.
-   *    Do this early, before we add headers, get busy, or execute the request.
-   */
-  readOnlyInterceptor();
-  /**
-   *  Log Http:
-   *    This logs all HTTP traffic.
-   *    Do this first so it can log the Http call happening in and out (last).
-   */
-  logHttpInterceptor();
+  const interceptors = [
+    /**
+     *  Log Http:
+     *    This logs all HTTP traffic.
+     *    Do this first so it can log the Http call happening in and out (last).
+     */
+    logHttpInterceptor,
+    /**
+     * ReadOnly:
+     *    This makes sure that HTTP POST, PUT and DELETE are not allowed if the
+     *    user does not have permission. If they do not, then it cancels the request.
+     *    Do this early, before we add headers, get busy, or execute the request.
+     */
+    readOnlyInterceptor,
+    /**
+       SSL:
+     *    Ensure SSL by making calls that use http instead use https.
+     */
+    ensureSSLInterceptor,
+    /**
+     * Auth:
+     *    Add the authentication headers.
+     */
+    authInterceptor,
+    /**
+     * CSRF:
+     *    Add the CSRF headers.
+     */
+    csrfInterceptor,
+    /**
+     *  Log headers:
+     *    Log all headers.
+     *    Must come after the headers are stuffed and before the request is made.
+     */
+    logHeadersInterceptor,
+    /**
+     *  Busy:
+     *    Enable and increment the count of HTTP requests, which can be used to show a busy indicator.
+     *    Also decrement the count when responses are received, to eventually turn off the busy indicator.
+     *    This must happen once it is certain a request will be made,
+     *    and right after the response is received.
+     */
+    busyInterceptor,
+    /**
+     * Transform Response:
+     *    Transform the response, making it easier to consume.
+     *    This could happen anywhere in this particular stream,
+     *    as it operates on the response.
+     */
+    transformInterceptor,
+  ];
+
+  interceptors.reverse().forEach((f) => f());
 }
