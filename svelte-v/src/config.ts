77c0051848507db3d1a9ac823ef1771019e3511c
John Papa <john@johnpapa.net>
2022-11-16 01:12:07 -0500

    api is set to port 5001

--- svelte-v/src/config.ts
+++ svelte-v/src/config.ts
@@ -1,14 +1,14 @@
 // const API: string = process.env.SVELTE_APP_API || 'api';
 // const DEV: boolean = !!process.env.DEV || true;
 
-const API: string = 'api';
+const API: string = 'http://localhost:5001/api';
 const DEV: boolean = true;
 
 function logRouteLocation(location: Object) {
-  // console.log(DEV);
-  if (process.env.ROLLUP_WATCH) {
-    console.table(location);
-  }
+  console.log(DEV);
+  // if (process.env.ROLLUP_WATCH) {
+  //   console.table(location);
+  // }
 }
 
 export { API, DEV, logRouteLocation };
